918882fc4e3c5e674c88bb0fdede1619dcb4138f
shiva <you@example.com>
2024-10-18 14:32:00 +0530

    first commit

---
+++ tkr/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function App() {
+  return (
+<>
+<h1>welcome to iqoo users</h1>
+    <input type="text" placeholder="enter name"/>
+    <input type="email" placeholder="enter mail"/>
+    <input type="date" placeholder="enter age" />
+    <input type="submit" button="submit" />
+    
+</>
+  )
+}
+
+export default App
